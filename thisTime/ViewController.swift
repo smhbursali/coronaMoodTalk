ca1cc8c6e884c23d84e4a3227cb7a20129698d52
semih <smhbursali@gmail.com>
2019-10-22 23:09:23 -0400

    Initial Commit

---
+++ thisTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thisTime
+//
+//  Created by semih bursali on 10/22/19.
+//  Copyright © 2019 semih bursali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
